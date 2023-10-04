861c0aac0519414c17a7d33d221038568e74b3fa
Mike Panitz <michpa@hotmail.com>
2023-10-03 18:44:04 -0700

    Initial Commit

---
+++ DebuggingAndProfiling/DebuggingAndProfilingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebuggingAndProfilingApp.swift
+//  DebuggingAndProfiling
+//
+//  Created by Mike Panitz on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct DebuggingAndProfilingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
