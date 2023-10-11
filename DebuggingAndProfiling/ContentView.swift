c4eeee46c0ffe2f0b725ddd9df903d9b105d216e
Tori Lew <torirlew@gmail.com>
2023-10-11 14:42:16 -0700

    Time Profiler to compare and contrast StressTest and the OccassionalUpdate views

--- DebuggingAndProfiling/ContentView.swift
+++ DebuggingAndProfiling/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     var body: some View {
         TabView {
             // 2 - The app redrew its views 7 times in the 30 sec OccassionalUpdate was in the foreground and 2,730 times while stress test was in the foreground
+            
+            // 3 - In the OccationalUpdate, starting took 19.82s in the 30sec it was in the foreground. In stressTest, it took 5.96 sec. So while the views were drawn more times during the stress test, those were updating faster so it didn't use as much CPU.
             OccassionalUpdate()
                 .tabItem {
                     Image(systemName: "smiley")
